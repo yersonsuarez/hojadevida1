4971a83c1170be12c08fb9290a697dafe0df3e03
yersonsuarez <yersonblancos@gmail.com>
2023-03-22 19:15:34 -0500

    ss

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hoja de vida | perfil</title>
+    <script src="https://kit.fontawesome.com/80573cd677.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="card text-center">
+            <div class="card-header">
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-4">
+                        <ol class="list-group list-group-numbered">
+                            <li class="list-group-item">An item</li>
+                            <li class="list-group-item">A second item</li>
+                            <li class="list-group-item">A third item</li>
+                            <li class="list-group-item">A fourth item</li>
+                            <li class="list-group-item">And a fifth one</li>
+                        </ol>
+                    </div>
+                    <div class="col-4">
+                        <div class="text-center">
+                            <img src="https://definicion.de/wp-content/uploads/2019/07/perfil-de-usuario.png" alt="" class="img-fluid rounded shadow" width="50%">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <img src="https://talently.tech/blog/wp-content/uploads/2022/02/%C2%BFQue-es-un-framework-en-programacion-scaled-1200x675.jpg" alt="" class="img-fluid rounded shadow">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="card text-center shadow">
+                            <div class="card-header">
+                              Perfil Profesional
+                            </div>
+                            <div class="card-body">
+                              <h5 class="card-title">Camilo Mancipe (Backend Developer Sr)</h5>
+                              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, id? Exercitationem numquam magnam quae amet, quam aliquam repellat molestiae nemo error recusandae modi deleniti minus, qui itaque facere ab molestias iure odit labore laboriosam voluptatibus saepe corporis unde praesentium. Iusto dicta delectus sequi. Recusandae exercitationem odio nulla iste, ipsa voluptatum error, autem voluptate provident blanditiis nemo at officia natus ea quae unde aliquid qui suscipit, sint dignissimos nostrum cupiditate maiores? Ratione dolores perspiciatis quidem illum impedit excepturi itaque. Sapiente reiciendis saepe ab, voluptate alias eius provident ad amet nulla nesciunt? Consequuntur vel commodi assumenda iusto qui suscipit eum quibusdam dolorum eius incidunt expedita architecto, veniam ratione maiores a quis ullam praesentium quaerat? Ipsa ut quod possimus voluptas eveniet. Fugiat ratione ab harum mollitia vitae incidunt quidem totam voluptates cum nemo quo quod quos impedit dolores, sint ad, velit eaque ducimus cupiditate aspernatur veritatis. Quibusdam unde quos corrupti nulla! Rerum, magni.
+                                Cumque magni enim error odio animi sequi ad magnam placeat inventore sint, doloremque beatae omnis distinctio, ea laudantium, amet corrupti? Numquam assumenda error voluptates suscipit rerum recusandae culpa ipsam mollitia, sit ratione nihil aperiam quod voluptatum hic minus! Eaque sunt pariatur a temporibus culpa aspernatur fugit laudantium repellat dolor odit, quam repudiandae accusantium eligendi accusamus voluptatem in modi quae. Recusandae, iste iure iusto illum a rerum, mollitia corporis et porro deserunt itaque doloribus laudantium perspiciatis libero cumque. Corrupti officia dignissimos illo fugiat quidem et provident. Sint soluta molestias doloribus exercitationem asperiores magnam nulla optio deserunt quis ullam. Vero, itaque. Explicabo architecto voluptate veritatis, illo recusandae nobis quam asperiores accusamus praesentium hic libero? Natus minus consectetur ullam veniam harum sapiente totam cum id rerum tempore laudantium illo, quis animi earum ipsum dolore maiores cupiditate odit voluptatem, vel nostrum quisquam nobis. Aliquid, a! Similique, repellendus? Molestiae obcaecati ipsum commodi vero placeat sequi ab? Recusandae cum, natus consectetur harum atque explicabo cumque autem minus impedit eligendi quasi inventore minima? Quibusdam odio nisi ea, porro ipsum magni? Ipsa magnam ipsum omnis enim eum laborum cupiditate assumenda culpa quasi vero, voluptatem eaque cumque illum provident quas, eligendi iste molestiae reiciendis qui velit corporis ut incidunt accusantium! Necessitatibus, aut suscipit nesciunt maiores quo voluptates explicabo aliquam tempore sunt delectus, commodi, in tenetur at laborum. Error consequuntur libero deserunt amet ab doloribus earum voluptatem porro dolore dolores. Sint numquam provident hic quo eligendi iusto ullam fugiat at! Ipsum obcaecati aliquam adipisci quia deserunt unde, aut voluptates quidem iure, corrupti impedit laudantium qui error tenetur possimus placeat labore delectus earum fugiat fuga ipsa repudiandae assumenda. Sed aspernatur commodi quibusdam nemo similique fuga animi sunt autem adipisci architecto sit odit illo fugiat labore enim, amet quidem nesciunt officiis veritatis, consectetur dolorem.</p>
+                            </div>
+                            <div class="card-footer text-muted">
+                              <a href="http://"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+                              <a href="mailto:camilo@gmail.com"><i class="fa-solid fa-envelope fa-2xl"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer text-muted">
+                <i class="fa-solid fa-copyright"></i> Todos los derechos reservados
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
